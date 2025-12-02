c9f5309c55d5a4c0ada532d6cc7b3cd89eaefb2c
akachan <i@neri.moe>
2025-12-03 02:25:14 +0900

    应该修好了 + bump version

--- src/index.ts
+++ src/index.ts
@@ -34,70 +34,76 @@ function getDeviceId(nameOrAlias: string, config: Config) {
 }
 
 export function apply(ctx: Context, config: Config) {
-  ctx.http.config.baseURL = config.url;
-  ctx.http.config.headers = {
-    "Authorization": `Bearer ${config.token}`,
-    "Content-Type": "application/json"
-  }
-  ctx.command('on <alias>')
-    .action(async (_, alias) => {
-      let dev = getDeviceId(alias, config);
-      if (await switch_on(ctx, config, dev.id)) {
-        return `${dev.name} 启动成功`;
-      } else {
-        return `${dev.name} 启动失败，内部错误，请联系管理人员`;
-      };
-    });
-  ctx.command('off <alias>')
-    .action(async (_, alias) => {
-      if (alias == "all") {
-        let promises = [];
-        let devs: { name: string, status: boolean }[] = [];
-        Object.entries(config.entities).map(([name, id]) => {
-          promises.push(switch_off(ctx, config, id));
-        });
-        let results = await Promise.all(promises);
-        results.map((res, idx) => { devs[idx].status = res });
-        let str = "";
-        devs.map((dev) => {
-          if (dev.status) {
-            str += `${name} 关闭成功\n`;
-          } else {
-            str += `${name} 关闭失败，内部错误，请联系管理人员\n`;
-          }
-        })
-      } else {
+  try {
+    ctx.http.config.baseURL = config.url;
+    ctx.http.config.headers = {
+      "Authorization": `Bearer ${config.token}`,
+      "Content-Type": "application/json"
+    }
+    ctx.command('on <alias>')
+      .action(async (_, alias) => {
         let dev = getDeviceId(alias, config);
-        if (await switch_off(ctx, config, dev.id)) {
-          return `${dev.name} 关闭成功`;
+        if (await switch_on(ctx, config, dev.id)) {
+          return `${dev.name} 启动成功`;
         } else {
-          return `${dev.name} 关闭失败，内部错误，请联系管理人员`;
+          return `${dev.name} 启动失败，内部错误，请联系管理人员`;
         };
-      }
-    });
-  ctx.command('show [alias]')
-    .action(async (__dirname, alias) => {
-      if (alias == "") {
-        let promises = [];
-        let devs: { name: string, status: string }[] = [];
-        Object.entries(config.entities).map(([name, id]) => {
-          promises.push(ctx.http.get(`/api/states/${id}`));
-          devs.push({ name, status: "unknown" });
-        });
-        let results = await Promise.all(promises);
-        results.map((res, idx) => {
-          devs[idx].status = res.state;
-        })
-        let str = "";
-        devs.map((dev) => {
-          str += `${dev.name}: ${dev.status}\n`
-        })
+      });
+    ctx.command('off <alias>')
+      .action(async (_, alias) => {
+        if (alias == "all") {
+          let promises = [];
+          let devs: { name: string, status: boolean }[] = [];
+          Object.entries(config.entities).forEach(([name, id]) => {
+            promises.push(switch_off(ctx, config, id));
+            devs.push({ name, status: false })
+          });
+          let results = await Promise.all(promises);
+          results.forEach((res, idx) => { devs[idx].status = res });
+          let str = "";
+          devs.forEach((dev) => {
+            if (dev.status) {
+              str += `${name} 关闭成功\n`;
+            } else {
+              str += `${name} 关闭失败，内部错误，请联系管理人员\n`;
+            }
+          })
+          return str;
+        } else {
+          let dev = getDeviceId(alias, config);
+          if (await switch_off(ctx, config, dev.id)) {
+            return `${dev.name} 关闭成功`;
+          } else {
+            return `${dev.name} 关闭失败，内部错误，请联系管理人员`;
+          };
+        }
+      });
+    ctx.command('show [alias]')
+      .action(async (__dirname, alias) => {
+        if (alias == undefined) {
+          let promises = [];
+          let devs: { name: string, status: string }[] = [];
+          Object.entries(config.entities).forEach(([name, id]) => {
+            promises.push(ctx.http.get(`/api/states/${id}`));
+            devs.push({ name, status: "unknown" });
+          });
+          let results = await Promise.all(promises);
+          results.forEach((res, idx) => {
+            devs[idx].status = res.state;
+          })
+          let str = "";
+          devs.forEach((dev) => {
+            str += `${dev.name}: ${dev.status}\n`
+          })
+          return str;
+        } else {
+          let dev = getDeviceId(alias, config);
+          let response = await ctx.http.get(`/api/states/${dev.id}`);
+          return `${dev.name} 当前状态：${response.state}`;
+        }
+      })
 
-      } else {
-        let dev = getDeviceId(alias, config);
-        let response = await ctx.http.get(`/api/states/${dev.id}`);
-        return `${dev.name} 当前状态：${response.state}`;
-      }
-    })
-
+  } catch (e) {
+    return e.message
+  }
 }
