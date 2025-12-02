a68cd48a918524bff31a450e3d12e3ba207569cc
akachan <i@neri.moe>
2025-12-03 01:57:43 +0900

    增加了主名和alias，可以show all和off all，不准备添加on all

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,33 @@ export const name = 'homeassistant'
 export interface Config {
   url: string,
   token: string,
-  entities: { [alias: string]: string }
+  entities: { [name: string]: string }
+  alias: { [alias: string]: string }
 }
 
 export const Config: Schema<Config> = Schema.object({
   url: Schema.string().required().description('Home Assistant 的 url'),
   token: Schema.string().required().description('长期访问令牌'),
-  entities: Schema.dict(Schema.string().required().description("实体ID"), Schema.string().required().description("实体别名")).role('table').description('实体列表')
+  entities: Schema.dict(
+    Schema.string().required().description("实体ID"),
+    Schema.string().required().description("实体名")).role('table').description('实体列表'),
+  alias: Schema.dict(
+    Schema.string().required().description("实体名"),
+    Schema.string().required().description("实体别名")).role('table').description('实体别名'),
 })
+
+function getDeviceId(nameOrAlias: string, config: Config) {
+  let entityId = config.entities[nameOrAlias];
+  let name = nameOrAlias;
+  if (entityId == null) {
+    name = config.alias[nameOrAlias];
+    entityId = config.entities[name];
+    if (entityId == null) {
+      throw Error("设备不存在")
+    }
+  };
+  return { name, id: entityId };
+}
 
 export function apply(ctx: Context, config: Config) {
   ctx.http.config.baseURL = config.url;
@@ -22,29 +41,63 @@ export function apply(ctx: Context, config: Config) {
   }
   ctx.command('on <alias>')
     .action(async (_, alias) => {
-      let entityId = config.entities[alias];
-      if (entityId == null) return "设备不存在";
-      if (await switch_on(ctx, config, entityId)) {
-        return `${alias} 启动成功`;
+      let dev = getDeviceId(alias, config);
+      if (await switch_on(ctx, config, dev.id)) {
+        return `${dev.name} 启动成功`;
       } else {
-        return `${alias} 启动失败，内部错误，请联系管理人员`;
+        return `${dev.name} 启动失败，内部错误，请联系管理人员`;
       };
     });
   ctx.command('off <alias>')
     .action(async (_, alias) => {
-      let entityId = config.entities[alias];
-      if (entityId == null) return "设备不存在";
-      if (await switch_off(ctx, config, entityId)) {
-        return `${alias} 关闭成功`;
+      if (alias == "all") {
+        let promises = [];
+        let devs: { name: string, status: boolean }[] = [];
+        Object.entries(config.entities).map(([name, id]) => {
+          promises.push(switch_off(ctx, config, id));
+        });
+        let results = await Promise.all(promises);
+        results.map((res, idx) => { devs[idx].status = res });
+        let str = "";
+        devs.map((dev) => {
+          if (dev.status) {
+            str += `${name} 关闭成功\n`;
+          } else {
+            str += `${name} 关闭失败，内部错误，请联系管理人员\n`;
+          }
+        })
       } else {
-        return `${alias} 关闭失败，内部错误，请联系管理人员`;
-      };
+        let dev = getDeviceId(alias, config);
+        if (await switch_off(ctx, config, dev.id)) {
+          return `${dev.name} 关闭成功`;
+        } else {
+          return `${dev.name} 关闭失败，内部错误，请联系管理人员`;
+        };
+      }
     });
-  ctx.command('show <alias>')
+  ctx.command('show [alias]')
     .action(async (__dirname, alias) => {
-      let entityId = config.entities[alias];
-      let response = await ctx.http.get(`/api/states/${entityId}`);
-      return `${alias} 当前状态：${response.state}`;
+      if (alias == "") {
+        let promises = [];
+        let devs: { name: string, status: string }[] = [];
+        Object.entries(config.entities).map(([name, id]) => {
+          promises.push(ctx.http.get(`/api/states/${id}`));
+          devs.push({ name, status: "unknown" });
+        });
+        let results = await Promise.all(promises);
+        results.map((res, idx) => {
+          devs[idx].status = res.state;
+        })
+        let str = "";
+        devs.map((dev) => {
+          str += `${dev.name}: ${dev.status}\n`
+        })
+
+      } else {
+        let dev = getDeviceId(alias, config);
+        let response = await ctx.http.get(`/api/states/${dev.id}`);
+        return `${dev.name} 当前状态：${response.state}`;
+      }
     })
 
 }
